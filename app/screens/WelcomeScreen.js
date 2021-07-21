515e75f6694320aacaba36ff567f42fa533d4b68
occiandiaali <ocean.diaali@gmail.com>
2021-07-21 20:17:10 +0100

    style buttons

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import {View, ImageBackground, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import AppButton from '../components/AppButton';
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
+      blurRadius={1}
       source={{
         uri: 'https://images.pexels.com/photos/1745747/pexels-photo-1745747.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       }}>
-      {/* <Image
-        style={styles.logo}
-        source={require('../assets/welcome.png')}
-        alt="logo"
-      /> */}
-      <View style={styles.loginBtn} />
-      <View style={styles.regBtn} />
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require('../assets/cash.png')} />
+        <Text style={styles.label}>Buy and Sell Anything!</Text>
+      </View>
+      <View style={styles.btnsContainer}>
+        <AppButton
+          title="Login"
+          onPress={() => console.log('Tapped login...')}
+        />
+        <AppButton title="Register" />
+      </View>
     </ImageBackground>
   );
 }
@@ -24,22 +38,26 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-end',
   },
-  loginBtn: {
-    width: '100%',
-    height: 70,
-    backgroundColor: 'teal',
+  btnsContainer: {
+    margin: 6,
   },
-  //   logo: {
-  //     width: 200,
-  //     height: 200,
-  //     position: 'absolute',
-  //     top: 50,
-  //     left: 100,
-  //   },
-  regBtn: {
-    width: '100%',
-    height: 70,
-    backgroundColor: 'tomato',
+  label: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    shadowColor: '#000',
+    color: '#FFF',
+  },
+  logo: {
+    width: 180,
+    height: 180,
+
+    alignSelf: 'center',
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 55,
+    justifyContent: 'center',
+    alignSelf: 'center',
   },
 });
 
