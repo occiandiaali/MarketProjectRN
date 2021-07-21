681b1fa055c071ee2c32b14c7b0845e88588adda
occiandiaali <ocean.diaali@gmail.com>
2021-07-21 20:17:40 +0100

    dry run components

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import Card from './app/components/Card';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 //import { useDeviceOrientation } from '@react-native-community/hooks';
 
 const App = () => {
-  // return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  //return <WelcomeScreen />;
+  // return <ViewImageScreen />;
+  return (
+    <Card
+      title="Lazy boy basket"
+      subTitle="=N=13, 500"
+      image={require('./app/assets/swinger.jpg')}
+    />
+  );
 };
 
 export default App;
