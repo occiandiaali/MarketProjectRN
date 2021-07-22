3c05d728cf3f53fd1acee31b3a58b794a47ef8e3
occiandiaali <ocean.diaali@gmail.com>
2021-07-22 11:58:36 +0100

    test listing component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 //import { useDeviceOrientation } from '@react-native-community/hooks';
@@ -8,13 +9,14 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 const App = () => {
   //return <WelcomeScreen />;
   // return <ViewImageScreen />;
-  return (
-    <Card
-      title="Lazy boy basket"
-      subTitle="=N=13, 500"
-      image={require('./app/assets/swinger.jpg')}
-    />
-  );
+  // return (
+  //   <Card
+  //     title="Lazy boy basket"
+  //     subTitle="=N=13, 500"
+  //     image={require('./app/assets/swinger.jpg')}
+  //   />
+  // );
+  return <ListingDetailsScreen />;
 };
 
 export default App;
