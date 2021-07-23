45fc9a64f7ddbee808717e17ebedc1650f23c070
occiandiaali <ocean.diaali@gmail.com>
2021-07-23 07:10:11 +0100

    test Message screen

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 //import { useDeviceOrientation } from '@react-native-community/hooks';
 
 const App = () => {
   //return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  // return <ViewImageScreen />;
   // return (
   //   <Card
   //     title="Lazy boy basket"
@@ -17,6 +18,7 @@ const App = () => {
   //   />
   // );
   //return <ListingDetailsScreen />;
+  return <MessagesScreen />;
 };
 
 export default App;
